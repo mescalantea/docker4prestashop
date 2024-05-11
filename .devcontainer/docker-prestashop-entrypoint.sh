77576d24c6b977f8ec21dbc3eea05c656f7f3be4
Michel Escalante <michel.escalantea@gmail.com>
2024-05-11 17:23:49 +0200

    Improved config

--- .devcontainer/docker-prestashop-entrypoint.sh
+++ .devcontainer/docker-prestashop-entrypoint.sh
@@ -21,23 +21,33 @@ if [ ! -f /var/www/html/.post-install-complete ]; then
     # Wait for database to be ready and then create it.
     echo "🔍 Waiting for database to be ready..."
     DB_PORT=3306
-    result=$(($(wait_for nc -z "${PS_DB_SERVER}" "${DB_PORT}")))
+    result=$(($(wait_for nc -z "${PS_DB_HOST}" "${PS_DB_PORT}")))
     if [ $result -eq 1 ]; then
-        echo "❌ ${PS_DB_SERVER}:${DB_PORT} is not available"
+        echo "❌ ${PS_DB_HOST}:${PS_DB_PORT} is not available"
         exit 1
     fi
 
     echo "🚀 Installing PrestaShop..."
-    
-    php install/index_cli.php \
-    --domain=${PS_DOMAIN} \
-    --db_server=${PS_DB_SERVER} \
-    --db_name=${PS_DB_NAME} \
-    --db_user=${PS_DB_USER} \
-    --db_password=${PS_DB_PASSWORD} \
-    --prefix=${PS_PREFIX} \
-    --email=${PS_EMAIL} \
-    --password=${PS_PASSWORD}
+
+    runuser -g www-data -u www-data -- php -d memory_limit=-1 /var/www/html/install/index_cli.php \
+        --domain="$PS_DOMAIN" \
+        --db_server=$PS_DB_HOST:$PS_DB_PORT \
+        --db_name="$PS_DB_NAME" \
+        --db_user=$PS_DB_USER \
+        --db_password=$PS_DB_PASSWORD \
+        --prefix="$PS_DB_PREFIX" \
+        --firstname="$PS_FIRSTNAME" \
+        --lastname="$PS_LASTNAME" \
+        --email="$PS_ADMIN_EMAIL" \
+        --password=$PS_ADMIN_PASSWORD \
+        --language=$PS_LANGUAGE \
+        --country=$PS_COUNTRY \
+        --all_languages=$PS_ALL_LANGUAGES \
+        --newsletter=0 \
+        --send_email=0 \
+        --ssl=$PS_ENABLE_SSL \
+        --timezone="$PS_TIMEZONE" \
+        --fixtures=$PS_FIXTURES
 
     rm -rf install
 
